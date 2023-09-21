86c5886055493fb3c6b8083916eb2e7a9045b7e2
Zeek98 <68119903+zeek98@users.noreply.github.com>
2023-09-21 10:02:52 +0500

    Update and rename app.py to main_app.py

--- main_app.py
+++ main_app.py
@@ -11,48 +11,12 @@ from sentence_transformers import SentenceTransformer
 import pinecone
 import openai
 import os
+from utils import load_environment_variables, find_match, query_refiner, get_conversation_string
 
 # Load environment variables
-load_dotenv()
-PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-PINECONE_ENV = os.getenv('PINECONE_ENVIRONMENT')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-openai.api_key = OPENAI_API_KEY
+load_environment_variables()
 
-# Initialize Sentence Transformer model
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-# Initialize Pinecone
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
-index = pinecone.Index('dp-index')
-
-# Utility functions
-def find_match(input):
-    input_em = model.encode(input).tolist()
-    result = index.query(input_em, top_k=2, includeMetadata=True)
-    return result['matches'][0]['metadata']['text'] + "\n" + result['matches'][1]['metadata']['text']
-
-def query_refiner(conversation, query):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    return response['choices'][0]['text']
-
-def get_conversation_string():
-    conversation_string = ""
-    for i in range(len(st.session_state['responses']) - 1):
-        conversation_string += "Human: " + st.session_state['requests'][i] + "\n"
-        conversation_string += "Bot: " + st.session_state['responses'][i + 1] + "\n"
-    return conversation_string
-
-# Main application
+# Main application function
 def main():
     st.set_page_config(page_title="Chat and PDF QA")
     st.header("Chat and PDF QA 💬📚")
