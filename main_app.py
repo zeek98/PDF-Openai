b7c1e3467056f6e2587717d17d5f41ba9027c2e6
Zeek98 <68119903+zeek98@users.noreply.github.com>
2023-09-21 10:49:54 +0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,6 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 from sentence_transformers import SentenceTransformer
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 import pinecone
 import openai
 import os
